9d261fdb59e3b6817dba7dab927f47223e2a27e0
ksenyaktch <ksenyaktch@gmail.com>
2020-12-12 19:42:46 +0500

    changed models

--- main/models.py
+++ main/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-#ПРИВЕТИКИ
+
+# обновила
+# обновляю
+# прив
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    # user_ID = models.CharField(max_length=255) автоматом
+    # budget_ID = models.CharField(max_length=255) обратные ссылки
+
 class CurrentBudget(models.Model):
-    user_ID = models.CharField(max_length=255)
-    budget_ID = models.CharField(max_length=255)
+    user_ID = models.ForeignKey(User, on_delete = models.CASCADE)
     moneySum = models.IntegerField(default = 0)
     DateOfStart = models.DateTimeField( null=True, blank=True)
     DateOfFinish = models.DateTimeField( null=True, blank=True)
@@ -11,14 +20,6 @@ class CurrentBudget(models.Model):
     RestSum = models.IntegerField(default = 0)
     AvaibleSumToday = models.IntegerField(default = 0)
 
-class Data(models.Model):
-    user_ID = models.CharField(max_length=255)
-    SpendSum = models.IntegerField(default = 0)
-    data = models.DateTimeField( null=True, blank=True)
 
-class User(models.Model):
-    user_ID = models.CharField(max_length=255)
-    budget_ID = models.CharField(max_length=255)
-    email = models.EmailField()
-    password = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
+
+
