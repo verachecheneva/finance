059d2cd52dcf5afd232617959bcb58b5733f0490
ksenyaktch <ksenyaktch@gmail.com>
2020-12-21 01:24:32 +0500

    test2

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, Client
+from django.test import Client, TestCase
 from django.urls import reverse
 import requests
 from .models import CurrentBudget, User
@@ -6,14 +6,21 @@ from .models import CurrentBudget, User
 class TestRegistrationProfile(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username = 'sarah', 
-                                            email = 'flower@yandex.ru',
-                                            password = 'qazwsx1234')
-    
+        self.user = User.objects._create_user(username='sarah', email='flower@gmail.com', password='qazwsx1234')
+
     def test_user_has_profile(self):
         self.client.force_login(self.user, backend=None)
         response = self.client.get(reverse('account'))
-        self.assertEqual(response.status_code, )
+        self.assertEqual(response.status_code, 200)
+    
+    def test_test_registered_user_new_budget(self):
+        self.client.force_login(self.user, backend=None)
+        self.client.post(reverse('budget_new'),{'new': 'Test', 'SpendToday': 222})
+        self.assertEqual(CurrentBudget.objects.count(), 1)
+        budget = CurrentBudget.objects.first()
+        self.assertEqual(budget.name, 'Test')
+        self.assertEqual(budget.user_ID, self.user)
+        self.assertEqual(budget.SpendToday, 222)
 
 
 
