8f8551cf709b80309b31421e47c82f71d2e8555d
ksenyaktch <ksenyaktch@gmail.com>
2020-12-21 14:27:36 +0500

    doc

--- main/views.py
+++ main/views.py
@@ -4,60 +4,47 @@ from django.urls import reverse
 from django.views.generic.base import View
 from .form import AddBudget
 
-"""объединяем html шаблон с контекстом для домашней страницы"""
+
 def home(request): 
+    """объединяем html шаблон с контекстом для домашней страницы"""
     """получаем все объекты из CurrentBudget и User"""
     budget_is = CurrentBudget.objects.all()
     user_is = User.objects.all()
     return render(request, 'blog/home.html', {'budgets':budget_is, 'Users': user_is})
 
-"""объединяем html шаблон с контекстом для страницы пользователя"""
+
 def account(request):
+    """объединяем html шаблон с контекстом для страницы пользователя"""
     """запрашиваем только id пользователя для определения данных на его странице"""
     budget_is = CurrentBudget.objects.filter(user_ID=request.user)
     return render(request, 'blog/account.html', {'budgets':budget_is })
 
-"""импортируем необходимую форму для регистрации"""
+
 from .form import SignUpForm
-"""регистрация"""
+
 def register(request):
-    """делаем переприсваивание"""
+    """страница регистрации, если регистрация проходит успешно пользователь переходит на следующую страницу, иначе остается на странице регистрации"""
     form_class = SignUpForm 
-    """Либо отправляем данные в теле запроса, либо ничего не передаем"""
     user_form = form_class(request.POST or None)
-    """если данные отправились в теле запроса"""
     if request.method == 'POST':
         if user_form.is_valid():
-            """если форма заполнена корректно, то мы ее присваиваем нововму пользователю и сохраняем нового пользователя"""
             new_user = user_form.save(commit=False)
-            """мы не сохраняем парот, а шифруем для созранения безопасности"""
             new_user.set_password(user_form.cleaned_data['password1'])
-            """необходимо самостоятельно сохранять потому что commit в значении false"""
             new_user.save()
-            """если регистрация прошла успешно, переходим на страницу, где нас уведомляют о регистрации"""
             return render(request, 'registration/register_done.html', {'new_user': new_user})
-    """если регистрация не прошла снова попадаем на старницу регистрации"""
     return render(request, 'registration/register.html', {'user_form': user_form})
 
 """аналогично импортируем необходимую форму для регистрации бюджета"""
 def budget_new(request):
-    """если мы отправляем данные в теле запроса"""
+    """регистрируем новый бюджет, если форма отправлена успешна переходим на страницу аккаунта, иначе остаемся на странице добавления бюджета"""
     if request.method == 'POST':
-        """в форму помещаем данные в теле запроса"""
         form = AddBudget(request.POST)
-        """если форма заполнена корректно"""
         if form.is_valid():
-            """значения бюджета в форме"""
             budget = form.save(commit=False)
-            """получаем id пользователя"""
             budget.user_ID = request.user
-            """сохраняем бюджет"""
             budget.save()
-            """бюджеты определенного пользователя будут доступны по id"""
             budget_is = CurrentBudget.objects.filter(user_ID=request.user)
-            """переходим в аккаунт если бюджет успешно доавлен """
             return render(request, 'blog/account.html', {'budgets':budget_is })
-        """если что-то не так, то снова попадаем на страницу создания бюджета"""
         return render(request, 'blog/budget_edit.html', {'budgets_form': form })
     form = AddBudget
     return render(request, 'blog/budget_edit.html', {'budget_form': form})
